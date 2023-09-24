2d52bdb1e976fcccea5d6d5ff83a7dbae9c23c32
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-24 23:12:49 +0300

    Added code to allow users to add tasks to their to-do list

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-let tabs = ["today", "work"];
-let classNames = ["active-tab"]
 
+let currentTab; 
+let tabs = ["today", "work"];
+
+let todayTasks = [];
+let workTasks = [];
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+app.post("/add_task_today", (req, res) => {
+   todayTasks.push(req.body["newTask"]);
+   for(let i = 0; i < todayTasks.length; i++){
+      console.log(todayTasks[i]);
+   }
+   res.render("index.ejs", {tab: currentTab, active: "active-tab", todaysTask: todayTasks});
+});
+
+app.post("/add_task_work", (req, res) => {
+   workTasks.push(req.body["newTask"]);
+   for(let i = 0; i < workTasks.length; i++){
+      console.log(workTasks[i]);
+   }
+   res.render("work.ejs", {tab: currentTab, active: "active-tab", worksTask: workTasks});
+});
+
 app.get("/work", (req,res) =>{
-   res.render("work.ejs", {tab: tabs[1], active: classNames[0]});
+   currentTab = "work";
+   res.render("work.ejs", {tab: currentTab, active: "active-tab"});
 });
 
 app.get("/", (req, res) => {
-   res.render("index.ejs", {tab: tabs[0], active: classNames[0]});
+   currentTab = "today";
+   res.render("index.ejs", {tab: currentTab, active: "active-tab"});
 });
 
 app.listen(port, ()=>{
