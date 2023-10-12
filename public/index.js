acb549343cf515036de0603e9cb17b0ba87f64c2
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-12 18:58:02 +0300

    redo the UI and remove bugs from code

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 $(".check").change(function(){
   let textId = $(this).data("text-id");
-  let textToChange = $(".task-text").eq(textId);
+  let textToChange = $(".task").eq(textId);
 
   if($(this).prop("checked")){
     textToChange.addClass("text-decoration-line-through");
