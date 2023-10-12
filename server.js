c50286439d09faa8b0b6c53d520e414e2a067c65
PARSIVALJ <uchejesusboy@gmail.com>
2023-10-12 14:00:03 +0300

    modify home page backend to store the task in the database and render it to the user when the server restarts

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
+let currentTab; 
+let tabs = ["today", "work"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let d = new Date();
+let fullDate = days[d.getDay()]+", "+months[d.getMonth()]+" "+d.getDate();
+
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB")
+   .then(()=>{
+      console.log("Connected to DB successfully.");
+   })
+   .catch(()=> {
+      console.log("Failed to connect to DB.");
+   });
 
 const tasksSchema = new mongoose.Schema({
    task:{
@@ -17,22 +34,28 @@ const tasksSchema = new mongoose.Schema({
 const Task = mongoose.model("Task", tasksSchema);
 const workTask = mongoose.model("workTask", tasksSchema);
 
-let currentTab; 
-let tabs = ["today", "work"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const task1 = new Task({
+   task: "Welcome to your todolist!"
+});
 
-let d = new Date();
-let fullDate = days[d.getDay()]+", "+months[d.getMonth()]+" "+d.getDate();
+const task2 = new Task({
+   task: "type in an item and hit the + button to add the item."
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+const task3 = new Task({
+   task: "<---- Hit this to delete an item."
+});
 
-app.post("/add_task_today", (req, res) => {
-   todayTasks.push(req.body["newTask"]);
-   console.log("Today: ")
-   console.log(todayTasks);
-   res.render("index.ejs", {tab: currentTab, active: "active-tab", todaysTask: todayTasks, date: fullDate});
+const defaultItems = [task1, task2, task3];
+
+app.post("/add_task_today", async (req, res) => {
+   const newTask = req.body.newTask;
+    
+   const task = new Task({
+     task: newTask
+   });
+   await task.save();
+   res.redirect("/");
 });
 
 app.post("/add_task_work", (req, res) => {
@@ -47,9 +70,22 @@ app.get("/work", (req,res) =>{
    res.render("work.ejs", {tab: currentTab, active: "active-tab", worksTask: workTasks, date: fullDate});
 });
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
    currentTab = tabs[0];
-   res.render("index.ejs", {tab: currentTab, active: "active-tab", todaysTask: todayTasks, date: fullDate});
+   const tasks = await Task.find({});
+   if(tasks.length === 0){
+      Task.insertMany(defaultItems, {ordered:true})
+        .then(() =>{
+           console.log(`Successful`);
+        })
+        .catch(() => {
+           console.log(`Failed`);
+        });;
+       res.redirect("/");  
+   } else{
+      res.render("index.ejs", {tab: currentTab, active: "active-tab", todaysTask: tasks, date: fullDate});
+   }
+   
 });
 
 app.listen(port, ()=>{
